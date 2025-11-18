b4aaa7dab61aa8622c737999971bbfee4ccd9756
Garrett Speed <g.t.speed@uu.nl>
2025-11-18 09:47:13 +0100

    Change daily statistic to take a method as a parameter.

--- inflammation/models.py
+++ inflammation/models.py
@@ -8,7 +8,7 @@ and each column represents a single day across all patients.
 """
 
 import numpy as np
-
+from typing import Union
 
 def load_csv(filename):  
     """Load a Numpy array from a CSV
@@ -17,18 +17,18 @@ def load_csv(filename):
     """
     return np.loadtxt(fname=filename, delimiter=',')
 
+valid_numpy_methods = Union[np.mean, np.max, np.min, np.std, np.median]
 
-def daily_mean(data):
-    """Calculate the daily mean of a 2d inflammation data array."""
-    return np.mean(data, axis=0)
+def daily_statistic(data, method: valid_numpy_methods=np.mean):
+    """Calculates a statistic on an Array using a numpy array statistic method. 
 
+    Args:
+        data (Array): 2D inflammation array
+        method (valid_numpy_methods, optional): A numpy statistic method that does 
+                not require any other arguments than the array and axis. 
+                Defaults to np.mean.
 
-def daily_max(data):
-    """Calculate the daily max of a 2d inflammation data array."""
-    return np.max(data, axis=0)
-
-
-def daily_min(data):
-    """Calculate the daily min of a 2d inflammation data array."""
-    return np.min(data, axis=0)
-
+    Returns:
+        Array: A statistic in an array. 
+    """
+    return method(data, axis=0)
