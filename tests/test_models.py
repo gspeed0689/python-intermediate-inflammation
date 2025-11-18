5b7dbaa2d59f6ab96669bd78bf249cb5b2b148fd
Garrett Speed <g.t.speed@uu.nl>
2025-11-18 11:16:51 +0100

    Added tests to check functionality of passing a parameter.

--- tests/test_models.py
+++ tests/test_models.py
@@ -4,40 +4,18 @@ import numpy as np
 import numpy.testing as npt
 import pytest
 
-from inflammation.models import daily_mean, daily_max, daily_min
+from inflammation.models import daily_statistic
 
 @pytest.mark.parametrize(
-    "test, expected",
+    "test, method, expected",
     [
-        ([ [0, 0], [0, 0], [0, 0] ], [0, 0]),
-        ([ [1, 2], [3, 4], [5, 6] ], [3, 4]),
+        ([ [0, 0], [0, 0], [0, 0] ], np.mean, [0, 0]),
+        ([ [1, 2], [3, 4], [5, 6] ], np.mean, [3, 4]),
+        ([ [0, 0], [0, 0], [0, 0] ], np.min, [0, 0]),
+        ([ [1, 2], [3, 4], [5, 6] ], np.min, [1, 2]),
+        ([ [0, 0], [0, 0], [0, 0] ], np.max, [0, 0]),
+        ([ [1, 2], [3, 4], [5, 6] ], np.max, [5, 6]),
     ])
-def test_daily_mean(test, expected):
+def test_method_expected(test, method, expected):
     """Test mean function works for array of zeroes and positive integers."""
-    npt.assert_array_equal(daily_mean(np.array(test)), np.array(expected))
-
-@pytest.mark.parametrize(
-    "test, expected",
-    [
-        ([ [0, 0], [0, 0], [0, 0] ], [0, 0]),
-        ([ [1, 2], [3, 4], [5, 6] ], [5, 6]),
-    ])
-def test_daily_max(test, expected):
-    """Test max function works for array of zeroes and positive integers."""
-    npt.assert_array_equal(daily_max(np.array(test)), np.array(expected))
-
-@pytest.mark.parametrize(
-    "test, expected",
-    [
-        ([ [0, 0], [0, 0], [0, 0] ], [0, 0]),
-        ([ [1, 2], [3, 4], [5, 6] ], [1, 2]),
-    ])
-def test_daily_min(test, expected):
-    """Test min function works for array of zeroes and positive integers."""
-    npt.assert_array_equal(daily_min(np.array(test)), np.array(expected))
-
-def test_daily_min_string():
-    """Test the daily_min function that if it is passed a string a TypeError is raised."""
-    with pytest.raises(TypeError):
-        test_input = [["Empty", "Coconuts"], ["European Swallow", "African Swallow"]]
-        error_expected = daily_min(test_input)
+    npt.assert_array_equal(daily_statistic(np.array(test), method), np.array(expected))
